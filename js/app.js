cd4b6a2abadcfb97b396d13222e2d6b04103f403
Jill Lopez <jlopez@Jills-MacBook-Pro-2.local>
2014-11-09 20:54:21 -0800

    added some behavior to the address book using angular. I added some code to create an angular module and controller. it reads the js/employees.js file and index.html can create an interactive address book with search and filter options.

--- js/app.js
+++ js/app.js
@@ -5,3 +5,19 @@
     Add code here to create a new Angular application and controller.
     The array of employee objects is already in a global variable named 'pawneeEmployees'
 */
+
+"use strict";
+
+angular.module('AddressBook', [])
+	.controller('AddressController', function($scope){
+		//initialize employees and order
+		$scope.employees = pawneeEmployees;
+		$scope.order = 'lastName';
+
+
+
+});
+
+
+
+
